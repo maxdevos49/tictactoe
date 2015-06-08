1ab5f94058349c5044b3534c7af3b8798ef0bb9d
Max DeVos <maxdevos49@gmail.com>
2015-06-08 00:38:14 -0500

    added buttons and outlet controllers

--- TicTacToeTeam/TicTacToeTeam/ViewController.swift
+++ TicTacToeTeam/TicTacToeTeam/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var TopLeftText: UIButton!
+    @IBOutlet weak var TopCenterText: UIButton!
+    @IBOutlet weak var TopRightText: UIButton!
+    @IBOutlet weak var MiddleLeftText: UIButton!
+    @IBOutlet weak var CenterText: UIButton!
+    @IBOutlet weak var MiddleRightText: UIButton!
+    @IBOutlet weak var BottomLeftText: UIButton!
+    @IBOutlet weak var BottomCenterText: UIButton!
+    @IBOutlet weak var BottomRightText: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
